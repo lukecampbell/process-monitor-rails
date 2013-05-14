51c1780d10994f098e953a76f6c6efc30350c620
Luke Campbell <luke.s.campbell@gmail.com>
2013-05-14 18:41:51 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProcessMonitor::Application.config.secret_token = 'b512095d9a8578a3ad84fb49a0f522d9ec8b336c015b9b8d5970e5e57329069ebf44a5d732b78edbeb67c770e301327619088bc4a0af13d5427ee33e4bee5e0f'
